2f4c057c06575bace073edf2335cc845f0b22ec7
Mikman83 <ivlev.michael@gmail.com>
2018-12-19 14:52:15 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+  $(document).ready(function(){
+  //   $("#sticker").sticky({
+  //     topSpacing: 500,
+  //     bottomSpacing: 0,
+  //     widthFromWrapper: true,
+  //   }); window.pageYOffset
+
+  $(window).scroll(function() {
+        if($(this).scrollTop() >= 5300 && $(this).scrollTop() <= 5900) {
+            $('#sticker').addClass('fixed');
+            $('#sticker').removeClass('bottom');
+        }
+        else{
+            $('#sticker').removeClass('fixed');
+             $('#sticker').addClass('bottom');
+        }
+    });
+  });
+
+
 AOS.init();
